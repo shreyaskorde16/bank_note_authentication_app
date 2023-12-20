d8d104e2c8eac99c2dfab6b2fb9092b206e3703e
shreyaskorde16 <shreyas.korde@student.uni-siegen.de>
2023-12-20 20:57:42 +0100

    3rd commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('BankNote_Authentication.csv')
 #print(df)
@@ -23,7 +24,14 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier()
 classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
 
 
-
+accuracy = accuracy_score(y_test, y_pred)
+#print(f'Accuracy after applying Random forest classifier is {"{:.2%}".format(accuracy)}')
+print('\n')
+print('*'*10)
+print(f'Accuracy after applying Random forest classifier is {accuracy:.2%}')
+print('-'*10)
+print('\n')
 
