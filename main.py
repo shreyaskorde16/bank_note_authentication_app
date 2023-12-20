e3e7334c5df5ca3e17f1ed8275a4abddeed385ba
shreyaskorde16 <shreyas.korde@student.uni-siegen.de>
2023-12-20 20:46:36 +0100

    second after random forest

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ df = pd.read_csv('BankNote_Authentication.csv')
 #print(df)
 x = df.iloc[:, :-1]
 y = df.iloc[:,-1]
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+
+#print(y_train)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier()
+classifier.fit(x_train, y_train)
+
+
+
+
